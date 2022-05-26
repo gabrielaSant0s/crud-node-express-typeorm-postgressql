f85e28c0e84856fcf6fcf589b27ff3858bb4be64
Gabriela S Santos <gbss.santos@poli.ufrj.br>
2022-05-26 15:45:22 -0300

    äutenticando rota de categories

--- src/services/routes.ts
+++ src/services/routes.ts
@@ -15,7 +15,11 @@ import authMiddleware from "../middlewares/authMiddleware";
 const routes = Router();
 
 routes.post("/categories", new CreateCategoryController().handle);
-routes.get("/categories", new GetAllCategoriesController().handle);
+routes.get(
+  "/categories",
+  authMiddleware,
+  new GetAllCategoriesController().handle
+);
 routes.delete("/categories/:id", new DeleteCategoryController().handle);
 routes.put("/categories/:id", new UpdateCategoryController().handle);
 
